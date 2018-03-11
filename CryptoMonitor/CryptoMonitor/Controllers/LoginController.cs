be643e3b3586fa519b35a1b38261f5c9c80663a1
Ayuub Adan <ayuubadan@outlook.com>
2018-03-11 21:07:43 +0000

    Created  a login page that is connected to the database

---
+++ CryptoMonitor/CryptoMonitor/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CryptoMonitor.Models.Login;
+
+namespace CryptoMonitor.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorise(User user)
+        {
+            using (CryptoMonitorEntities db = new CryptoMonitorEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    user.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", user);
+                }
+                else
+                {
+                    Session["userId"] = userDetails.UserId;
+                    return RedirectToAction("Index", "Dashboard", userDetails);
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
