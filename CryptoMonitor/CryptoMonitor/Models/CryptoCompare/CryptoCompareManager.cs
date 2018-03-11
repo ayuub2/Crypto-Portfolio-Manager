42e5a2e22dba0ed7d0d58c41dae86095186dc149
matthewdovey <matthewdovey@hotmail.co.uk>
2018-03-11 18:26:31 +0000

    Initial creation of crypto compare manager

---
+++ CryptoMonitor/CryptoMonitor/Models/CryptoCompare/CryptoCompareManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CryptoMonitor.Models.CryptoCompare
+{
+    public class CryptoCompareManager
+    {
+        private const string BaseUrl = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR";
+
+        private readonly HttpClient _httpClient = new HttpClient();
+    }
+}
