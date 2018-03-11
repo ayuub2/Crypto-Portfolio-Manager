415af7681f78169987b95f66a7dacf942b0ce435
matthewdovey <matthewdovey@hotmail.co.uk>
2018-03-11 18:45:44 +0000

    further development

--- CryptoMonitor/CryptoMonitor/Models/CryptoCompare/CryptoCompareManager.cs
+++ CryptoMonitor/CryptoMonitor/Models/CryptoCompare/CryptoCompareManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace CryptoMonitor.Models.CryptoCompare
 {
@@ -12,5 +13,18 @@ namespace CryptoMonitor.Models.CryptoCompare
         private const string BaseUrl = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR";
 
         private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task GetPriceFor(string coin) 
+        {
+            var uri = BaseUrl;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, new Uri(uri));
+
+            var response = await _httpClient.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            //return JsonConvert.DeserializeObject<MarketSummaryRequest<MarketSummaryResult[]>>(json);
+        }
     }
 }
